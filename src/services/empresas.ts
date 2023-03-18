9ce0402c17c9227a01508ff844d371470dd14dc9
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-17 23:10:51 -0400

    Refactor SucursalModel find operation to return specified fields and populate with parent Empresa fields

--- src/services/empresas.ts
+++ src/services/empresas.ts
@@ -1,4 +1,5 @@
 import { JwtPayload } from "jsonwebtoken";
+import { ApiError } from "../class/ApiError";
 import { Empresa, Sucursal } from "../interfaces/empresa.interface";
 import { EmpresaModel, SucursalModel } from "../models/empresa.model";
 
@@ -8,7 +9,7 @@ export const getAllEmpresas = async (session: JwtPayload) => {
 }
 
 export const getAllSucursales = async (id: string) => {
-    const listAll = await SucursalModel.find({ empresa: id })
+    const listAll = await SucursalModel.find({ empresa: id }, { "_id": 1, "name": 1 })
     return listAll
 }
 
@@ -18,8 +19,14 @@ export const detailEmpresa = async (id: string) => {
 }
 
 export const detailSucursal = async (id: string) => {
-    const detail = await SucursalModel.findOne({ _id: id })
-    return detail
+    return new Promise((resolve, reject) => {
+        SucursalModel.find({ _id: id }, { "createdAt": 0, "updatedAt": 0 })
+            .populate('empresa', '-createdAt -updatedAt -_id -propietario')
+            .exec((err, data) => {
+                if (err) throw new ApiError(500, "Ocurrio un error interno")
+                resolve(data)
+            })
+    })
 }
 
 export const createEmpresa = async (data: Empresa, session: JwtPayload) => {
