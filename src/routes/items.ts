f9bc06df0901724d4779b1dfffcf9397281dc0f4
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-21 01:32:17 -0400

    Add error handling middleware to item routes.

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { verificacionDeToken } from "../middleware/session.middleware";
+import { errorHandle } from "../utils/error.handle";
 
 const router = Router()
 
-//
-router.get('/',)
-router.get('/:id',)
-router.post('/',)
-router.put('/:id',)
-router.delete('/:id',)
+router.get('/sucursal/:sucursalId', errorHandle(verificacionDeToken),)
+router.get('/:itemId', errorHandle(verificacionDeToken),)
+router.post('/sucursal/:sucursalId', errorHandle(verificacionDeToken),)
+router.put('/:itemId', errorHandle(verificacionDeToken),)
+router.delete('/:itemId', errorHandle(verificacionDeToken),)
 
 export { router }
