a201d98d00402694c24bae7623a698869c4732e0
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-21 02:12:11 -0400

    Add controllers for items routes.

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,13 +1,14 @@
+import { listAllItemsController, detailItemController, createItemController, updateItemController, deleteItemController } from './../controllers/items';
 import { Router } from "express";
 import { verificacionDeToken } from "../middleware/session.middleware";
 import { errorHandle } from "../utils/error.handle";
 
 const router = Router()
 
-router.get('/sucursal/:sucursalId', errorHandle(verificacionDeToken),)
-router.get('/:itemId', errorHandle(verificacionDeToken),)
-router.post('/sucursal/:sucursalId', errorHandle(verificacionDeToken),)
-router.put('/:itemId', errorHandle(verificacionDeToken),)
-router.delete('/:itemId', errorHandle(verificacionDeToken),)
+router.get('/sucursal/:sucursalId', errorHandle(verificacionDeToken), listAllItemsController)
+router.get('/:itemId', errorHandle(verificacionDeToken), detailItemController)
+router.post('/sucursal/:sucursalId', errorHandle(verificacionDeToken), createItemController)
+router.put('/:itemId', errorHandle(verificacionDeToken), updateItemController)
+router.delete('/:itemId', errorHandle(verificacionDeToken), deleteItemController)
 
 export { router }
