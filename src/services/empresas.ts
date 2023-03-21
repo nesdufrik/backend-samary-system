e8b32b1525c9722dfc78f4af8662be267110ad13
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-20 21:09:52 -0400

    Remove unused import of ApiError and update populate query for detailSucursal.

--- src/services/empresas.ts
+++ src/services/empresas.ts
@@ -1,5 +1,4 @@
 import { JwtPayload } from "jsonwebtoken";
-import { ApiError } from "../class/ApiError";
 import { Empresa, Sucursal } from "../interfaces/empresa.interface";
 import { EmpresaModel, SucursalModel } from "../models/empresa.model";
 
@@ -9,7 +8,7 @@ export const getAllEmpresas = async (session: JwtPayload) => {
 }
 
 export const getAllSucursales = async (id: string) => {
-    const listAll = await SucursalModel.find({ empresa: id }, { "createdAt": 0, "updatedAt": 0, "empresa": 0 })
+    const listAll = await SucursalModel.find({ empresa: id }, { "createdAt": 0, "updatedAt": 0 })
     return listAll
 }
 
@@ -19,14 +18,8 @@ export const detailEmpresa = async (id: string) => {
 }
 
 export const detailSucursal = async (id: string) => {
-    return new Promise((resolve, reject) => {
-        SucursalModel.findOne({ _id: id }, { "createdAt": 0, "updatedAt": 0 })
-            .populate('empresa', '-createdAt -updatedAt -_id -propietario')
-            .exec((err, data) => {
-                if (err) throw new ApiError(500, "Ocurrio un error interno")
-                resolve(data)
-            })
-    })
+    const detail = await SucursalModel.findOne({ _id: id }, { "createdAt": 0, "updatedAt": 0 })
+    return detail
 }
 
 export const createEmpresa = async (data: Empresa, session: JwtPayload) => {
