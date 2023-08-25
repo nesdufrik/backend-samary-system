6cfff9584d2f6e4a89277cb80271383a1e6b0026
Marco Antonio Humberto Decormis Pinto <nesdufrik@gmail.com>
2023-08-24 21:35:30 -0400

    [CREATE] Pagination to items endpoint

--- src/services/items.ts
+++ src/services/items.ts
@@ -1,4 +1,4 @@
-import { deleteFile, getFileStream, getUrlCdn, uploadFile } from '../config/s3'
+import { deleteFile, getUrlCdn, uploadFile } from '../config/s3'
 import { Item } from '../interfaces/transaccion.interface'
 import { ItemModel } from '../models/transaccion.model'
 import { EmpleadoModel } from '../models/usuario.model'
@@ -6,146 +6,189 @@ import { randomImageName } from '../utils/random.handle'
 import { ApiError } from '../class/ApiError'
 import sharp from 'sharp'
 
-export const getAllItems = async (id: string) => {
-    const listItems = await ItemModel.find(
-        { sucursal: id },
-        { createdAt: 0, updatedAt: 0, descripcion: 0, sucursal: 0 }
-    ).sort({ categoria: 1, etiqueta: 1, name: 1 })
+export const getAllItems = async (
+	id: string,
+	page: number,
+	cat: string,
+	sub: string
+) => {
+	let listItems: any
 
-    for (const item of listItems) {
-        if (item.image) {
-            item.image = getUrlCdn(item.image)
-        }
-    }
+	const options = {
+		select: {
+			createdAt: 0,
+			updatedAt: 0,
+			descripcion: 0,
+			sucursal: 0,
+		},
+		sort: {
+			categoria: 1,
+			etiqueta: 1,
+			name: 1,
+		},
+		page: page,
+		limit: 10,
+	}
 
-    return listItems
+	if (cat !== 'all' && sub !== 'all') {
+		listItems = await ItemModel.paginate(
+			{ sucursal: id, categoria: cat, etiqueta: sub },
+			options
+		)
+	}
+
+	if (cat !== 'all' && sub === 'all') {
+		listItems = await ItemModel.paginate(
+			{ sucursal: id, categoria: cat },
+			options
+		)
+	}
+
+	if (cat === 'all' && sub !== 'all') {
+		listItems = await ItemModel.paginate(
+			{ sucursal: id, etiqueta: sub },
+			options
+		)
+	}
+
+	if (cat === 'all' && sub === 'all') {
+		listItems = await ItemModel.paginate({ sucursal: id }, options)
+	}
+
+	for (const item of listItems.docs) {
+		if (item.image) {
+			item.image = getUrlCdn(item.image)
+		}
+	}
+
+	return listItems
 }
 
 export const detailItem = async (id: string) => {
-    const detail = await ItemModel.findOne(
-        { _id: id },
-        { createdAt: 0, updatedAt: 0, descripcion: 0, sucursal: 0 }
-    )
-    return detail
+	const detail = await ItemModel.findOne(
+		{ _id: id },
+		{ createdAt: 0, updatedAt: 0, descripcion: 0, sucursal: 0 }
+	)
+	return detail
 }
 
 export const createItem = async (data: Item, file: any, id: string) => {
-    let dataItem = {}
+	let dataItem = {}
 
-    if (file) {
-        //image random name
-        const imageName = randomImageName()
-        //resize image
-        const buffer = await sharp(file.buffer)
-            .resize({ width: 1920, height: 1080, fit: 'inside' })
-            .toBuffer()
-        const fileUpload = {
-            buffer,
-            filename: imageName,
-            mimetype: file.mimetype,
-        }
-        await uploadFile(fileUpload)
-        dataItem = {
-            name: data.name,
-            sucursal: id,
-            categoria: data.categoria,
-            etiqueta: data.etiqueta,
-            precio: data.precio,
-            image: imageName,
-        }
-    } else {
-        dataItem = {
-            name: data.name,
-            sucursal: id,
-            categoria: data.categoria,
-            etiqueta: data.etiqueta,
-            precio: data.precio,
-        }
-    }
+	if (file) {
+		//image random name
+		const imageName = randomImageName()
+		//resize image
+		const buffer = await sharp(file.buffer)
+			.resize({ width: 1920, height: 1080, fit: 'inside' })
+			.toBuffer()
+		const fileUpload = {
+			buffer,
+			filename: imageName,
+			mimetype: file.mimetype,
+		}
+		await uploadFile(fileUpload)
+		dataItem = {
+			name: data.name,
+			sucursal: id,
+			categoria: data.categoria,
+			etiqueta: data.etiqueta,
+			precio: data.precio,
+			image: imageName,
+		}
+	} else {
+		dataItem = {
+			name: data.name,
+			sucursal: id,
+			categoria: data.categoria,
+			etiqueta: data.etiqueta,
+			precio: data.precio,
+		}
+	}
 
-    const createItem = await ItemModel.create(dataItem)
+	const createItem = await ItemModel.create(dataItem)
 
-    if (createItem.image) {
-        createItem.image = getUrlCdn(createItem.image)
-    }
+	if (createItem.image) {
+		createItem.image = getUrlCdn(createItem.image)
+	}
 
-    return createItem
+	return createItem
 }
 
 export const updateItem = async (data: Item, file: any, id: string) => {
-    const checkIs = await ItemModel.findOne({ _id: id })
+	const checkIs = await ItemModel.findOne({ _id: id })
 
-    if (!checkIs) throw new ApiError(404, 'Producto no encontrado')
+	if (!checkIs) throw new ApiError(404, 'Producto no encontrado')
 
-    if (file) {
-        //resize image
-        const buffer = await sharp(file.buffer)
-            .resize({ width: 1920, height: 1080, fit: 'inside' })
-            .toBuffer()
+	if (file) {
+		//resize image
+		const buffer = await sharp(file.buffer)
+			.resize({ width: 1920, height: 1080, fit: 'inside' })
+			.toBuffer()
 
-        //random name
-        const imageName = randomImageName()
+		//random name
+		const imageName = randomImageName()
 
-        if (checkIs.image) {
-            await deleteFile(checkIs.image)
-            const fileUpload = {
-                buffer,
-                filename: imageName,
-                mimetype: file.mimetype,
-            }
-            await uploadFile(fileUpload)
-            data.image = imageName
-        }
+		if (checkIs.image) {
+			await deleteFile(checkIs.image)
+			const fileUpload = {
+				buffer,
+				filename: imageName,
+				mimetype: file.mimetype,
+			}
+			await uploadFile(fileUpload)
+			data.image = imageName
+		}
 
-        if (!checkIs.image) {
-            const fileUpload = {
-                buffer,
-                filename: imageName,
-                mimetype: file.mimetype,
-            }
-            await uploadFile(fileUpload)
-            data.image = imageName
-        }
-    }
+		if (!checkIs.image) {
+			const fileUpload = {
+				buffer,
+				filename: imageName,
+				mimetype: file.mimetype,
+			}
+			await uploadFile(fileUpload)
+			data.image = imageName
+		}
+	}
 
-    if (!file) {
-        if (checkIs.image) {
-            data.image = checkIs.image
-        }
-    }
+	if (!file) {
+		if (checkIs.image) {
+			data.image = checkIs.image
+		}
+	}
 
-    const actualizar = await ItemModel.findOneAndUpdate({ _id: id }, data, {
-        new: true,
-    })
+	const actualizar = await ItemModel.findOneAndUpdate({ _id: id }, data, {
+		new: true,
+	})
 
-    if (actualizar?.image) {
-        actualizar.image = getUrlCdn(actualizar.image)
-    }
+	if (actualizar?.image) {
+		actualizar.image = getUrlCdn(actualizar.image)
+	}
 
-    return actualizar
+	return actualizar
 }
 
 export const deleteItem = async (id: string) => {
-    const checkIs = await ItemModel.findOne({ _id: id })
-    if (!checkIs) throw new ApiError(404, 'Producto no encontrado')
-    if (checkIs.image) await deleteFile(checkIs.image)
-    const eliminar = await ItemModel.deleteOne({ _id: id })
-    return eliminar
+	const checkIs = await ItemModel.findOne({ _id: id })
+	if (!checkIs) throw new ApiError(404, 'Producto no encontrado')
+	if (checkIs.image) await deleteFile(checkIs.image)
+	const eliminar = await ItemModel.deleteOne({ _id: id })
+	return eliminar
 }
 
 // Servicios para POS Employee
 
 export const posGetAllItems = async (id: string) => {
-    const sucursalId = await EmpleadoModel.findOne({ _id: id })
-    const listItems = await ItemModel.find(
-        { sucursal: sucursalId?.sucursal },
-        { createdAt: 0, updatedAt: 0, descripcion: 0, sucursal: 0 }
-    )
+	const sucursalId = await EmpleadoModel.findOne({ _id: id })
+	const listItems = await ItemModel.find(
+		{ sucursal: sucursalId?.sucursal },
+		{ createdAt: 0, updatedAt: 0, descripcion: 0, sucursal: 0 }
+	)
 
-    for (const item of listItems) {
-        if (item.image) {
-            item.image = getUrlCdn(item.image)
-        }
-    }
-    return listItems
+	for (const item of listItems) {
+		if (item.image) {
+			item.image = getUrlCdn(item.image)
+		}
+	}
+	return listItems
 }
